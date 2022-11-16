e05127f1e459be9341baf0e546f1af2da0285555
Imad Fayad <imad.fayad.if@gmail.com>
2022-11-16 13:22:48 +0100

    initial commit

---
+++ Stock-trading-2/Controllers/AksjeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stock_trading_2.Models;
+using System.Collections.Generic;
+
+namespace Stock_trading_2.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AksjeController : ControllerBase
+    {
+        public List<Aksje> hentAlle()
+        {
+            var aksjene = new List<Aksje>();
+
+            var akjse1 = new Aksje();
+            akjse1.Navn = "Apple";
+            akjse1.Pris = 150;
+            akjse1.Antall = 2;
+
+            var akjse2 = new Aksje();
+            akjse2.Navn = "Amazon";
+            akjse2.Pris = 98;
+            akjse2.Antall = 2;
+
+            aksjene.Add(akjse1);
+            aksjene.Add(akjse2);
+
+            return aksjene;
+        }
+    }
+}
---
+++ Stock-trading-2/Models/Aksje.cs
@@ -0,0 +1,10 @@
+﻿namespace Stock_trading_2.Models
+{
+    public class Aksje
+    {
+        // public int Id { get; set; }
+        public string Navn { get; set; }
+        public int Pris { get; set; }
+        public int Antall { get; set; }
+    }
+}
